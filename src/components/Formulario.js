33b698cab6271b1e4910b1c1307f0eb720e7c715
Pablo Avelar <jp.avelar.m96@gmail.com>
2020-12-12 18:09:16 -0600

    Agregando hook useCriptomoneda

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -37,9 +38,14 @@ const Formulario = () => {
     MONEDAS
   );
 
+   // utilizar useCriptomoneda
+   const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '');
+
   return ( 
     <form>
       <SelectMonedas />
+
+      <SelectCripto />
 
       <Boton
         type="submit"
